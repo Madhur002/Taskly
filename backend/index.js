12969e2c2e4f63157b3e40d014dc19f898f9346e
madhur <mad@gmail.com>
2023-05-06 21:13:01 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,26 @@
 import connectToMongo from "./db.js";
 import express from "express";
-import authRoutes from './routes/auth.js';
-import todosRoutes from './routes/todos.js';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import authRoutes from "./routes/auth.js";
+import todosRoutes from "./routes/todos.js";
+import cors from "cors";
+import dotenv from "dotenv";
 dotenv.config();
 
-
 connectToMongo();
-const app = express()
-const port = process.env.PORT || 5000
+const app = express();
+const port = process.env.PORT || 5000;
 app.use(express.json());
-app.use(cors());
-// AVAILABLE ROUTES
-app.use('/api/auth', authRoutes);
-app.use('/api/todos', todosRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Hello World this is me !')
-})
+app.use(cors());
+
+// AVAILABLE ROUTES
+app.use("/api/auth", authRoutes);
+app.use("/api/todos", todosRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello World this is me !");
+});
 
 app.listen(port, () => {
-  console.log(`listening on port ${port}`)
-})
-
+  console.log(`listening on port ${port}`);
+});
