1a4f6b1b1d14175f7e637ad2524390b30ef50c5d
madhur <mad@gmail.com>
2023-05-06 21:30:54 +0530

    updated new files

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,9 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use(cors());
+app.use(cors({
+  origin: 'https://taskly-backend.onrender.com/'
+}));
 
 // AVAILABLE ROUTES
 app.use("/api/auth", authRoutes);
